69a9d22e761b3509390fd9bdfa70a00a0b8e49fe
teejusb <5017202+teejusb@users.noreply.github.com>
2020-09-05 10:22:39 -0700

    Center the ECS logo in ScreenLogo underlay

--- BGAnimations/ScreenLogo underlay.lua
+++ BGAnimations/ScreenLogo underlay.lua
@@ -1,5 +1,14 @@
 -- ECS Logo
-return LoadActor(THEME:GetPathG("", "_logos/ecs (doubleres).png"))..{
+local af = Def.ActorFrame{
+	InitCommand=function(self)
+		self:Center()
+	end,
+	OffCommand=function(self) self:linear(0.5):diffusealpha(0) end,
+}
+
+af[#af+1] = LoadActor(THEME:GetPathG("", "_logos/ecs (doubleres).png"))..{
 	InitCommand=function(self) self:x(2):zoom(0.5):shadowlength(0.75) end,
 	OffCommand=function(self) self:linear(0.5):shadowlength(0) end
 }
+
+return af
