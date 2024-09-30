b648b199dbdb353e9a161d933fcb2960920937cd
teejusb <5017202+teejusb@users.noreply.github.com>
2024-09-30 14:42:47 -0700

    Update default songs

--- BGAnimations/ScreenSelectPlayMode out.lua
+++ BGAnimations/ScreenSelectPlayMode out.lua
@@ -5,23 +5,23 @@ return Def.Actor{
 
 		if ECS.Mode == "ECS" then
 			if GetDivision() == "upper" then
-				local default = SONGMAN:FindSong("ECS13 - Upper/[24] [240] For Creatures Such As We FB 246")
+				local default = SONGMAN:FindSong("ECS13 - Upper/[24] [240] Opasnyje Jadjernaja")
 				if default then
 					GAMESTATE:SetPreferredSong(default)
 				end
 			elseif GetDivision() == "mid" then
-				local default = SONGMAN:FindSong("ECS13 - Mid/[18] [180] Doomsday")
+				local default = SONGMAN:FindSong("ECS13 - Mid/[18] [180] Fractal Glitch Works (Part 3)")
 				if default then
 					GAMESTATE:SetPreferredSong(default)
 				end
 			else
-				local default = SONGMAN:FindSong("ECS13 - Lower/[12] [120] Colorless Eyes")
+				local default = SONGMAN:FindSong("ECS13 - Lower/[12] [120] Adventure")
 				if default then
 					GAMESTATE:SetPreferredSong(default)
 				end
 			end
 		elseif ECS.Mode == "Speed" then
-			local default = SONGMAN:FindSong("ECS13 - Speed/[24] [240] For Creatures Such As We FB 246")
+			local default = SONGMAN:FindSong("ECS13 - Speed/[24] [240] Satanic Static")
 			if default then
 				GAMESTATE:SetPreferredSong(default)
 			end
